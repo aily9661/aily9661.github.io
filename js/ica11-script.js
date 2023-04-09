3639cf5d87f16705e4c8a279203fdfd58fdc638a
aily9661 <aily9661@colorado.edu>
2023-04-09 10:41:35 -0600

    idk it works

--- js/ica11-script.js
+++ js/ica11-script.js
@@ -1,12 +1,13 @@
 const btn = document.querySelector('button');
 const url = 'https://random-word-api.herokuapp.com/word';
 const quoteText = document.querySelector('#js-quote-text');
+const loadScreen = document.querySelector("#loadScreen")
 const tweetOpen =[' Today I was ', ' Yesterday I was in a store doing ', ' I was hanging out with my buddies when '];
 const tweetCenter = [' Then I saw a man ', ' But I totally forgot to ', ' That\'s when I realized '];
 const tweetClose = [' That\'s how I know ', ' This taught me an important lesson in ', ' Now next time I know to '];
-let apiReturn = '';
-let word2 = '';
-let word3 = '';
+let apiReturn = '["monkeys"]';
+let word2 = '["monograhm"]';
+let word3 = '["xray"]';
 
 /* I use this delay function to allow for api to regenerate I am using this delay function from author, Descope on "Mastering JS" https://masteringjs.io/tutorials/fundamentals/wait-1-second-then */
 function delay(time) {
@@ -33,22 +34,21 @@ function getWord(){
     return response.text();
     })
     .then((text) => apiReturn=text);
-    delay(100).then(() => getWord());
+    delay(500).then(() => getWord());
     return apiReturn;
 }
 
 function getQuote(){
+    loadScreen.setAttribute('class','loadScreen');
+    delay(3000).then(() => loadScreen.removeAttribute('class','loadScreen'));
     let open = randomNum(tweetOpen);
     let center = randomNum(tweetCenter);
     let close = randomNum(tweetClose);
     let word1 = getWord();
-    delay(200).then(() => word2 = getWord());
-    delay(400).then(() => word3 = getWord());
-    word1.replace('["','');
-    word2.replace('["','');
-    word3.replace('["','');
+    delay(1000).then(() => word2 = getWord());
+    delay(2000).then(() => word3 = getWord());
     let newQuote = open + word1 + '.' + center + word2 + '.' + close + word3 + '.';
-    delay(600).then(() => displayQuote(newQuote));
+    delay(3000).then(() => displayQuote(newQuote));
 }
 
 getQuote();
